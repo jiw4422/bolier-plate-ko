ed97ce7375b10abbe304f5abd216c8656f927e84
jiw4422 <jiw4422@nate.com>
2020-05-10 13:53:41 +0900

    update source

---
+++ config/prod.js
@@ -0,0 +1,3 @@
+module.exports = {
+    mongoURI : 'mongodb+srv://jiwoong:jiwoong@jwmongodb-vdgbh.mongodb.net/test?retryWrites=true&w=majorityc'
+}
--- index.js
+++ index.js
@@ -1,13 +1,34 @@
-const express = require('express')
+const express = require('express');
 const app = express()
 const port = 5000
 
-const mongoose = require('mongoose')
-mongoose.connect('mongodb+srv://jiwoong:jiwoong@jwmongodb-vdgbh.mongodb.net/test?retryWrites=true&w=majority', {
+const {User} = require("./models/Users");
+const bodyParser = require('body-parser');
+
+const mongoose = require('mongoose');
+const config = require('./config/key');
+
+//application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({extended:true}));
+//application/json
+app.use(bodyParser.json()); 
+
+mongoose.connect( config.mongoURI , {
     useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true, useFindAndModify: false
 }).then(() => console.log('MongoDb connected'))
   .catch(err => console.log(err))
 
-app.get('/', (req, res) => res.send('Hello World!'))
+app.get('/', (req, res) => res.send('Hello World!dfdfdfdf'))
+
+app.post('/register' , (req, res) => {
+    //회원 가입 정보 들을 client에서 가져오면 그것들을 데이터베이스에 넣어준다.
+    const user = new User(req.body)
+    user.save((err, doc) => {
+        if(err) return res.json({success:false,err})
+        return res.status(200).json({
+            success:true
+        })
+    })
+})
 
 app.listen(port, () => console.log(`Example app listening at http://localhost:${port}`))
